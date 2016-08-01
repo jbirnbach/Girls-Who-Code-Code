80c263fe17710f197052289b6cf1a60d0d22d67c
Julia <jbirnbach18@gmail.com>
2016-08-01 14:06:27 -0400

    this is my code

--- ss3.py
+++ ss3.py
@@ -1,9 +1,12 @@
 import pygame
 import random
+from PIL import Image
 from blockk import *
 from cityscroller import *
 pygame.init()
 
+pygame.image.load("sky2.jpeg")
+im = pygame.image.load("sky2.jpeg")
 screen_width = 700
 screen_height = 500
 screen = pygame.display.set_mode([screen_width, screen_height])
@@ -25,6 +28,7 @@ lives = 6
 
 city = []
 x = 0
+sky_pos = 0
 
 ## The scroller object is created here
 #any_scroller = Scroller(screen_width, 300, screen_height, (255, 100, 100), 2)
@@ -46,6 +50,10 @@ while not done:
                 
     # Clear the screen
     screen.fill(WHITE)
+    sky = pygame.image.load('sky2.jpeg')
+    screen.blit(sky, (sky_pos, 0))
+
+
     building = Building(BLACK, random.randint(100,800), random.randint(25, 50), 800, random.randint(100, 600))
     city.append(building)
     for building in city:
@@ -78,9 +86,8 @@ while not done:
     for block in pink_blocks_hit_list:
         lives -= 1
         
-    if lives <= 0:
-    	screen.fill(RED)
-    	text = font.render("Game Over", True, WHITE)
+    if lives <= -6:
+    	screen.fill(BLACK)
 
     #Creates the scrote variables using pretty font    
     score_text = font.render("Score: " +str(score), True, BLACK)
@@ -88,16 +95,24 @@ while not done:
 
     #keeps the text on the page at these coordinates
     screen.blit(score_text, [500, 50])
-    screen.blit(lives_text, [50, 50])
+    screen.blit(lives_text, [50, 50])   
 
-    
     # Draw the scrolling background
     #Add your scrolling background here
     #any_scroller.draw_buildings(screen)
     #any_scroller.move_buildings()
 
+    #trying to add an image
+    
+    #sky.set_colorkey((255,255,255))
+    #sky.get_rect()
+    #sky.rect.x = 350
+    #sky.rect.y = 400
+
     # Draw all the sprites  
     all_sprites_list.draw(screen)
+
+    sky_pos -= 1
 
     # Go ahead and update the screen with what we've drawn.
     pygame.display.flip()
